165cedc10422236bccddcafe6f58329ef0a4b6fb
gongzaiawa <117426708+gongzaiawa@users.noreply.github.com>
2023-03-04 03:25:06 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-const ws = require("nodejs-websocket");
-let count = 0;
-const serve = ws.createServer(conn => {console.log('新的连接');count++conn.userName = `用户${count}号`broadcast(`${conn.userName}进入了聊天室`);// 接收到了浏览器的数据
-                                       conn.on('text', data => {broadcast(`${conn.userName}:` + data)})// 关闭的时候触发
-                                       conn.on('close', data => {console.log('关闭连接');broadcast(`${conn.userName}离开了聊天室`);})// 发生异常 触发
-                                       conn.on('error', data => {console.log('发生异常');})
-})
-// 广播信息给所有的用户
-function broadcast(msg) {serve.connections.forEach(item => {item.send(msg)})
-}serve.listen(3000, () => {console.log('监听端口3000')
-})
+@ServerEndpoint("/robin")
+public class ChatEndPoint {
+
+    private static Set<ChatEndPoint> webSocketSet = new HashSet<>();
+
+    private Session session;
+
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException {
+        System.out.println("接收的消息是：" + message);
+        System.out.println(session);
+        //将消息发送给其他的用户
+        for (Chat chat : webSocketSet) {
+            if(chat != this) {
+                chat.session.getBasicRemote().sendText(message);
+            }
+        }
+    }
+
+    @OnOpen
+    public void onOpen(Session session) {
+        this.session = session;
+        webSocketSet.add(this);
+    }
+
+    @OnClose
+    public void onClose(Session seesion) {
+        System.out.println("连接关闭了。。。");
+    }
+
+    @OnError
+    public void onError(Session session,Throwable error) {
+        System.out.println("出错了。。。。" + error.getMessage());
+    }
+}
