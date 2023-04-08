ed02daeb7914ecf98bfd3f6239f5d4c6e1d5040f
gongzaiawa <117426708+gongzaiawa@users.noreply.github.com>
2023-04-08 06:28:43 +0000

    Update gethttp.js

--- gethttp.js
+++ gethttp.js
@@ -1,86 +1,9 @@
-
-const https = require('https');
-
-
-function sendHTTPSRequest(url, method, headers, body, callback) {
-  const options = {
-    method: method,
-    headers: headers
-  };
-
-
-  if (body) {
-    options.body = body;
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://example.com/api/data", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+    var response = xhr.responseText;
+    console.log(response);
   }
-
-  const req = https.request(url, options, (res) => {
-    let data = '';
-
-
-    res.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    res.on('end', () => {
-
-      callback(data);
-    });
-  });
-
-
-  req.on('error', (err) => {
-    console.error(err);
-  });
-
-
-  if (body) {
-    req.write(body);
-  }
-
-
-  req.end();
-}
-
-
-Scratch.extensions.register('myExtension', {
-  getInfo: () => ({
-    id: 'myExtension',
-    name: 'My Extension',
-    blocks: [
-      {
-        opcode: 'sendHTTPSRequest',
-        blockType: Scratch.BlockType.COMMAND,
-        text: 'Send HTTPS request to [URL] with method [METHOD] and headers [HEADERS] and body [BODY]',
-        arguments: {
-          URL: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: ''
-          },
-          METHOD: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: 'GET'
-          },
-          HEADERS: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: '{}'
-          },
-          BODY: {
-            type: Scratch.ArgumentType.STRING,
-            defaultValue: ''
-          }
-        }
-      }
-    ]
-  }),
-
-  sendHTTPSRequest: (args) => {
-    const url = args.URL;
-    const method = args.METHOD;
-    const headers = JSON.parse(args.HEADERS);
-    const body = args.BODY;
-
-    sendHTTPSRequest(url, method, headers, body, (data) => {
-      console.log(data);
-    });
-  }
-});
+};
+xhr.send();
