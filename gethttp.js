60130e15e0e7f3933ab713b376578866a9689af8
gongzaiawa <117426708+gongzaiawa@users.noreply.github.com>
2023-04-12 10:46:43 +0000

    Update gethttp.js

--- gethttp.js
+++ gethttp.js
@@ -1,9 +1,31 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://example.com/api/data", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    var response = xhr.responseText;
-    console.log(response);
+(function (ext) {
+
+  function fetchUrl(url) {
+    return fetch(url)
+      .then(response => response.text());
   }
-};
-xhr.send();
+
+  ext.getHttps = function (url, callback) {
+    // Use the fetchUrl() function to make the GET request
+    fetchUrl(url)
+      .then(data => callback(data));
+  };
+
+  // Describe the extension metadata
+  ext._getStatus = function () {
+    return {status: 2, msg: 'Ready'};
+  };
+  ext._shutdown = function () {};
+
+  // Register the extension
+  const descriptor = {
+    blocks: [
+      ['R', 'fetch URL %s', 'getHttps', 'https://example.com'],
+    ],
+    displayName: 'HTTPS Fetch',
+    menuIconURI: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAQElEQVR42mNkwAL+L/A/0/iFjICuhDxDJdgogZkhP8HAgMDAwMCxSwTAKIDq3//GtHHsQPTCGOpgwoXMjgiiLEBZCE91AvAjtBVkJawMA0DUFC1cLWlgAAAAASUVORK5CYII=',
+    id: 'getHttps'
+  };
+  ScratchExtensions.register('HTTPS Fetch', descriptor, ext);
+
+})({});
