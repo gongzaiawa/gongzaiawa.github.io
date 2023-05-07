2bd2fa30b3c523dcc43245a3653d0ff2b074782c
gongzaiawa <117426708+gongzaiawa@users.noreply.github.com>
2023-05-07 04:34:39 +0000

    Update email.js

--- email.js
+++ email.js
@@ -1,17 +1,26 @@
 class EmailExtension {
-  constructor () {
+  constructor() {
     this.menuId = 'recipientMenu'
   }
 
-  getInfo () {
+  getInfo() {
+    const locale = {
+      en: {
+        sendEmail: 'Send email to %recipient with subject %subject and body %body'
+      },
+      zh_CN: {
+        sendEmail: '向邮箱 %recipient 发送标题为 %subject 内容为 %body 的邮件'
+      }
+    }
+
     return {
       id: 'email',
-      name: '邮箱验证码',
+      name: {en: 'Email Verification Code', zh_CN: '邮箱验证码'},
       blocks: [
         {
           opcode: 'sendEmail',
           blockType: Scratch.BlockType.COMMAND,
-          text: '发送 邮箱 [recipient] 上的主题为 [subject] 内容为 [body] 的邮件',
+          text: locale[Scratch.GUI_LANGUAGE]['sendEmail'],
           arguments: {
             recipient: {
               type: Scratch.ArgumentType.STRING,
@@ -23,7 +32,7 @@ class EmailExtension {
             },
             body: {
               type: Scratch.ArgumentType.STRING,
-              defaultValue: '内容'
+              defaultValue: 'Content'
             }
           }
         }
@@ -34,15 +43,25 @@ class EmailExtension {
     }
   }
 
-  async sendEmail (args) {
+  async sendEmail(args) {
     const baseUrl = 'https://v.api.aa1.cn/api/qqemail/new/?to='
     const url = `${baseUrl}"${args.recipient}"&subject=${args.subject}&message="${args.body}"`
     try {
       await fetch(url)
+      alert(locale[Scratch.GUI_LANGUAGE]['waitMessage'])
     } catch (error) {
       console.log("Error while sending email:", error)
     }
   }
 }
 
-Scratch.extensions.register(new EmailExtension())
+const locale = {
+  en: {
+    waitMessage: 'Please wait for a minute before using this feature again.'
+  },
+  zh_CN: {
+    waitMessage: '请稍等一分钟后再使用此功能。'
+  }
+}
+
+Scratch.extensions.register(new EmailExtension(locale))
